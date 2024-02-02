b35f0a633abf6b68b12ade3c6197e06b79b44bb1
pandya3033 <pandyapreet456@gmail.com>
2024-02-02 12:29:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
+  const [weight, setWeight] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [bmi, setBMI] = useState(0);
+  const [result, setResult] = useState();
+
+  // Formula : BMI = Weight(kg)/height(m)^2
+
+  let calculateBMI = (e) => {
+    e.preventDefault();
+    if (!weight || !height) {
+      alert("Enter Valid Weight And Height.");
+    } 
+    else{
+      let bmi = parseFloat(weight) / (parseFloat(height) * parseFloat(height));
+      setBMI(bmi.toFixed(2));
+
+      if (bmi < 18.5) {
+        setResult("You Are Underweight ☠️");
+      } else if (18.5 <= bmi && bmi < 24.9) {
+        setResult("You Are Healthyweight 😇");
+      } else if (25 <= bmi && bmi < 29.9) {
+        setResult("You Are Overweight! 🐖");
+      } else {
+        setResult("You Are Obese!!! 🐘");
+      }
+    }
+  };
+
+  function resetBTN() {
+    setBMI(0);
+    setWeight(0);
+    setHeight(0);
+    setResult();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <h2>BMI Calculator</h2>
+        <form id="form" onSubmit={calculateBMI}>
+          <div className="input-group mb-3">
+            <span className="input-group-text" id="inputGroup-sizing-default">
+              Weight (kg)
+            </span>
+            <input
+              type="text"
+              className="form-control"
+              aria-label="Sizing example input"
+              aria-describedby="inputGroup-sizing-default"
+              placeholder="Enter Your Weight"
+              value={weight}
+              onChange={(e) => setWeight(e.target.value)}
+            />
+          </div>
+          <div className="input-group mb-3">
+            <span className="input-group-text" id="inputGroup-sizing-default">
+              Height (m)
+            </span>
+            <input
+              type="text"
+              className="form-control"
+              aria-label="Sizing example input"
+              aria-describedby="inputGroup-sizing-default"
+              placeholder="Enter Your Height"
+              value={height}
+              onChange={(event) => setHeight(event.target.value)}
+            />
+          </div>
+          <div className="d-grid gap-2 col-12 mx-auto">
+            <button className="btn btn-outline-info">Submit</button>
+            <button
+              type="reset"
+              className="btn btn-outline-warning"
+              onClick={resetBTN}
+            >
+              Reset
+            </button>
+          </div>
+          <div>
+            <h4>Your BMI Is: {bmi}</h4>
+            <p>{result}</p>
+          </div>
+        </form>
+      </div>
+    </>
   );
 }
 
